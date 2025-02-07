97b14cecba396d36dfb515a9b67372db3bedf01c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-07 23:04:10 +0100

    feat: Implement missing methods in container wrappers, implement WorldlyContainer

--- common/src/main/java/net/blay09/mods/balm/api/container/DefaultContainer.java
+++ common/src/main/java/net/blay09/mods/balm/api/container/DefaultContainer.java
@@ -1,14 +1,17 @@
 package net.blay09.mods.balm.api.container;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
-public class DefaultContainer implements ImplementedContainer {
+public class DefaultContainer implements ImplementedContainer, WorldlyContainer {
 
     private NonNullList<ItemStack> items;
 
@@ -31,5 +34,24 @@ public class DefaultContainer implements ImplementedContainer {
 
     public CompoundTag serialize(HolderLookup.Provider provider) {
         return serializeInventory(provider);
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction direction) {
+        final var slots = new int[items.size()];
+        for (int i = 0; i < slots.length; i++) {
+            slots[i] = i;
+        }
+        return slots;
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int slot, ItemStack itemStack, @Nullable Direction direction) {
+        return true;
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int slot, ItemStack itemStack, Direction direction) {
+        return true;
     }
 }
