38499ddc242b3a72037564d064b07a0316277987
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-07 23:04:10 +0100

    feat: Implement missing methods in container wrappers, implement WorldlyContainer

--- common/src/main/java/net/blay09/mods/balm/api/container/CombinedContainer.java
+++ common/src/main/java/net/blay09/mods/balm/api/container/CombinedContainer.java
@@ -1,12 +1,17 @@
 package net.blay09.mods.balm.api.container;
 
+import net.minecraft.core.Direction;
 import net.minecraft.world.Container;
+import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public class CombinedContainer implements Container, ExtractionAwareContainer {
+public class CombinedContainer implements Container, WorldlyContainer, ExtractionAwareContainer {
     private final Container[] containers;
     private final int[] baseIndex;
     private final int totalSlots;
@@ -40,9 +45,12 @@ public class CombinedContainer implements Container, ExtractionAwareContainer {
         return index >= 0 && index < containers.length ? containers[index] : EmptyContainer.INSTANCE;
     }
 
-    private int getSlotFromIndex(int slot, int index) {
+    private int getInnerSlotFromIndex(int slot, int index) {
         return index > 0 && index < baseIndex.length ? slot - baseIndex[index - 1] : slot;
+    }
 
+    private int getOuterSlotFromIndex(int slot, int index) {
+        return index < baseIndex.length ? slot - baseIndex[index] : slot;
     }
 
     @Override
@@ -59,28 +67,28 @@ public class CombinedContainer implements Container, ExtractionAwareContainer {
     public ItemStack getItem(int slot) {
         int containerIndex = getContainerIndexForSlot(slot);
         Container container = getContainerFromIndex(containerIndex);
-        return container.getItem(getSlotFromIndex(slot, containerIndex));
+        return container.getItem(getInnerSlotFromIndex(slot, containerIndex));
     }
 
     @Override
     public ItemStack removeItem(int slot, int amount) {
         int containerIndex = getContainerIndexForSlot(slot);
         Container container = getContainerFromIndex(containerIndex);
-        return container.removeItem(getSlotFromIndex(slot, containerIndex), amount);
+        return container.removeItem(getInnerSlotFromIndex(slot, containerIndex), amount);
     }
 
     @Override
     public ItemStack removeItemNoUpdate(int slot) {
         int containerIndex = getContainerIndexForSlot(slot);
         Container container = getContainerFromIndex(containerIndex);
-        return container.removeItemNoUpdate(getSlotFromIndex(slot, containerIndex));
+        return container.removeItemNoUpdate(getInnerSlotFromIndex(slot, containerIndex));
     }
 
     @Override
     public void setItem(int slot, ItemStack itemStack) {
         int containerIndex = getContainerIndexForSlot(slot);
         Container container = getContainerFromIndex(containerIndex);
-        container.setItem(getSlotFromIndex(slot, containerIndex), itemStack);
+        container.setItem(getInnerSlotFromIndex(slot, containerIndex), itemStack);
     }
 
     @Override
@@ -107,7 +115,73 @@ public class CombinedContainer implements Container, ExtractionAwareContainer {
         int containerIndex = getContainerIndexForSlot(slot);
         Container container = getContainerFromIndex(containerIndex);
         if (container instanceof ExtractionAwareContainer extractionAwareContainer) {
-            return extractionAwareContainer.canExtractItem(getSlotFromIndex(slot, containerIndex));
+            return extractionAwareContainer.canExtractItem(getInnerSlotFromIndex(slot, containerIndex));
+        }
+        return true;
+    }
+
+    @Override
+    public void startOpen(Player player) {
+        for (Container container : containers) {
+            container.startOpen(player);
+        }
+    }
+
+    @Override
+    public void stopOpen(Player player) {
+        for (Container container : containers) {
+            container.stopOpen(player);
+        }
+    }
+
+    @Override
+    public boolean canPlaceItem(int slot, ItemStack itemStack) {
+        int containerIndex = getContainerIndexForSlot(slot);
+        Container container = getContainerFromIndex(containerIndex);
+        return container.canPlaceItem(getInnerSlotFromIndex(slot, containerIndex), itemStack);
+    }
+
+    @Override
+    public boolean canTakeItem(Container container, int slot, ItemStack itemStack) {
+        int containerIndex = getContainerIndexForSlot(slot);
+        Container subContainer = getContainerFromIndex(containerIndex);
+        return container.canTakeItem(subContainer, getInnerSlotFromIndex(slot, containerIndex), itemStack);
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction direction) {
+        Set<Integer> slots = new HashSet<>();
+        for (int index = 0; index < containers.length; index++) {
+            Container container = containers[index];
+            if (container instanceof WorldlyContainer worldlyContainer) {
+                for (int i : worldlyContainer.getSlotsForFace(direction)) {
+                    slots.add(i);
+                }
+            } else {
+                for (int i = 0; i < container.getContainerSize(); i++) {
+                    slots.add(getOuterSlotFromIndex(i, index));
+                }
+            }
+        }
+        return slots.stream().mapToInt(i -> i).toArray();
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int slot, ItemStack itemStack, @Nullable Direction direction) {
+        int containerIndex = getContainerIndexForSlot(slot);
+        Container container = getContainerFromIndex(containerIndex);
+        if (container instanceof WorldlyContainer worldlyContainer) {
+            return worldlyContainer.canPlaceItemThroughFace(slot, itemStack, direction);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int slot, ItemStack itemStack, Direction direction) {
+        int containerIndex = getContainerIndexForSlot(slot);
+        Container container = getContainerFromIndex(containerIndex);
+        if (container instanceof WorldlyContainer worldlyContainer) {
+            return worldlyContainer.canTakeItemThroughFace(slot, itemStack, direction);
         }
         return true;
     }
