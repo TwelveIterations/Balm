a269d4ac92036bc8a16bc5aee1b6969fe7e9b486
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-16 10:41:24 +0100

    feat: Add findEquipped and findAllEquipped to Curios/Trinkets support layer #121

--- fabric/src/main/java/net/blay09/mods/balm/fabric/compat/trinkets/FabricBalmModSupportTrinkets.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/compat/trinkets/FabricBalmModSupportTrinkets.java
@@ -5,6 +5,7 @@ import net.blay09.mods.balm.api.compat.trinkets.BalmModSupportTrinkets;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class FabricBalmModSupportTrinkets implements BalmModSupportTrinkets {
@@ -15,4 +16,21 @@ public class FabricBalmModSupportTrinkets implements BalmModSupportTrinkets {
         }
         return false;
     }
+
+    @Override
+    public ItemStack findEquipped(Player player, Predicate<ItemStack> predicate) {
+        if (Balm.isModLoaded("trinkets")) {
+            return TrinketsModCompat.findEquipped(player, predicate);
+        }
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public List<ItemStack> findAllEquipped(Player player, Predicate<ItemStack> predicate) {
+        if (Balm.isModLoaded("trinkets")) {
+            return TrinketsModCompat.findAllEquipped(player, predicate);
+        }
+        return List.of();
+    }
+
 }
