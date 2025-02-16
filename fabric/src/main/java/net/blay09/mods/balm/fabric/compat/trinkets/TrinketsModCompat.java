7a1a0910222343abbff6e6ce40878c171c9adc8c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-16 10:41:24 +0100

    feat: Add findEquipped and findAllEquipped to Curios/Trinkets support layer #121

--- fabric/src/main/java/net/blay09/mods/balm/fabric/compat/trinkets/TrinketsModCompat.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/compat/trinkets/TrinketsModCompat.java
@@ -1,13 +1,28 @@
 package net.blay09.mods.balm.fabric.compat.trinkets;
 
 import dev.emi.trinkets.api.TrinketsApi;
+import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class TrinketsModCompat {
     public static boolean isEquipped(Player player, Predicate<ItemStack> predicate) {
         return TrinketsApi.getTrinketComponent(player).map(trinkets -> trinkets.isEquipped(predicate)).orElse(false);
+    }
+
+    public static ItemStack findEquipped(Player player, Predicate<ItemStack> predicate) {
+        return TrinketsApi.getTrinketComponent(player)
+                .flatMap(trinkets -> trinkets.getEquipped(predicate).stream().findFirst())
+                .map(Tuple::getB)
+                .orElse(ItemStack.EMPTY);
+    }
+
+    public static List<ItemStack> findAllEquipped(Player player, Predicate<ItemStack> predicate) {
+        return TrinketsApi.getTrinketComponent(player)
+                .map(trinkets -> trinkets.getEquipped(predicate).stream().map(Tuple::getB).toList())
+                .orElse(List.of());
     }
 }
