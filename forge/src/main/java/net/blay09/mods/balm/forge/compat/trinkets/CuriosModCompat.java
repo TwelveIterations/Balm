389cd6600f06fa6abaf5aa0bbd5f4916ffc9d3dd
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-16 10:41:24 +0100

    feat: Add findEquipped and findAllEquipped to Curios/Trinkets support layer #121

--- forge/src/main/java/net/blay09/mods/balm/forge/compat/trinkets/CuriosModCompat.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/compat/trinkets/CuriosModCompat.java
@@ -3,11 +3,24 @@ package net.blay09.mods.balm.forge.compat.trinkets;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotResult;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class CuriosModCompat {
     public static boolean isEquipped(Player player, Predicate<ItemStack> predicate) {
         return CuriosApi.getCuriosInventory(player).map(trinkets -> trinkets.isEquipped(predicate)).orElse(false);
+    }
+
+    public static ItemStack findEquipped(Player player, Predicate<ItemStack> predicate) {
+        return CuriosApi.getCuriosInventory(player).resolve().flatMap(trinkets -> trinkets.findFirstCurio(predicate)).map(SlotResult::stack).orElse(ItemStack.EMPTY);
+    }
+
+    public static List<ItemStack> findAllEquipped(Player player, Predicate<ItemStack> predicate) {
+        return CuriosApi.getCuriosInventory(player)
+                .resolve()
+                .map(trinkets -> trinkets.findCurios(predicate).stream().map(SlotResult::stack).toList())
+                .orElse(List.of());
     }
 }
--- forge/src/main/java/net/blay09/mods/balm/forge/compat/trinkets/ForgeBalmModSupportTrinkets.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/compat/trinkets/ForgeBalmModSupportTrinkets.java
@@ -5,6 +5,7 @@ import net.blay09.mods.balm.api.compat.trinkets.BalmModSupportTrinkets;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class ForgeBalmModSupportTrinkets implements BalmModSupportTrinkets {
@@ -14,5 +15,21 @@ public class ForgeBalmModSupportTrinkets implements BalmModSupportTrinkets {
             return CuriosModCompat.isEquipped(player, predicate);
         }
         return false;
+    }
+
+    @Override
+    public ItemStack findEquipped(Player player, Predicate<ItemStack> predicate) {
+        if (Balm.isModLoaded("curios")) {
+            return CuriosModCompat.findEquipped(player, predicate);
+        }
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public List<ItemStack> findAllEquipped(Player player, Predicate<ItemStack> predicate) {
+        if (Balm.isModLoaded("curios")) {
+            return CuriosModCompat.findAllEquipped(player, predicate);
+        }
+        return List.of();
     }
 }
