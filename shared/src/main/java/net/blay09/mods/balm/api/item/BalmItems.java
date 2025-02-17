275a9a10b6fd0b2c3f4c021e06f9fed8ff5a31a8
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-17 23:17:30 +0100

    fix: Fix compile error

--- shared/src/main/java/net/blay09/mods/balm/api/item/BalmItems.java
+++ shared/src/main/java/net/blay09/mods/balm/api/item/BalmItems.java
@@ -1,11 +1,15 @@
 package net.blay09.mods.balm.api.item;
 
 import net.blay09.mods.balm.api.DeferredObject;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Comparator;
