65777d8ba71758fb26157cda5efc9be8f033d1d5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-18 10:46:41 +0100

    fix: Fix compile error on NeoForge

--- neoforge/src/main/java/net/blay09/mods/balm/neoforge/compat/NeoForgeBalmModSupport.java
+++ neoforge/src/main/java/net/blay09/mods/balm/neoforge/compat/NeoForgeBalmModSupport.java
@@ -1,14 +1,22 @@
 package net.blay09.mods.balm.neoforge.compat;
 
 import net.blay09.mods.balm.api.compat.BalmModSupport;
+import net.blay09.mods.balm.api.compat.hudinfo.BalmModSupportHudInfo;
 import net.blay09.mods.balm.api.compat.trinkets.BalmModSupportTrinkets;
+import net.blay09.mods.balm.common.compat.hudinfo.CommonBalmModSupportHudInfo;
 import net.blay09.mods.balm.neoforge.compat.trinkets.NeoForgeBalmModSupportTrinkets;
 
 public class NeoForgeBalmModSupport implements BalmModSupport {
     private final NeoForgeBalmModSupportTrinkets trinkets = new NeoForgeBalmModSupportTrinkets();
+    private final CommonBalmModSupportHudInfo hudInfo = new CommonBalmModSupportHudInfo();
 
     @Override
     public BalmModSupportTrinkets trinkets() {
         return trinkets;
+    }
+
+    @Override
+    public BalmModSupportHudInfo hudInfo() {
+        return hudInfo;
     }
 }
