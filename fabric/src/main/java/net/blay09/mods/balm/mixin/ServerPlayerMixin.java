a81ea752625b5bc3afc7f90ac41c3c0358edabe1
Ampflower 🌺 <git@ampflower.gay>
2025-02-24 12:41:17 -0800

    Use Share local for fromDimHolder

--- fabric/src/main/java/net/blay09/mods/balm/mixin/ServerPlayerMixin.java
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/ServerPlayerMixin.java
@@ -1,5 +1,7 @@
 package net.blay09.mods.balm.mixin;
 
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.event.PlayerChangedDimensionEvent;
 import net.blay09.mods.balm.api.event.PlayerOpenMenuEvent;
@@ -7,14 +9,12 @@ import net.blay09.mods.balm.api.event.TossItemEvent;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.portal.TeleportTransition;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -24,9 +24,6 @@ import java.util.OptionalInt;
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin {
 
-    @Unique
-    private static final ThreadLocal<ResourceKey<Level>> fromDimHolder = new ThreadLocal<>();
-
     @Inject(method = "openMenu(Lnet/minecraft/world/MenuProvider;)Ljava/util/OptionalInt;", at = @At("RETURN"))
     public void openMenu(@Nullable MenuProvider menuProvider, CallbackInfoReturnable<OptionalInt> callbackInfo) {
         ServerPlayer player = (ServerPlayer) (Object) this;
@@ -34,17 +31,17 @@ public class ServerPlayerMixin {
     }
 
     @Inject(method = "teleport(Lnet/minecraft/world/level/portal/TeleportTransition;)Lnet/minecraft/server/level/ServerPlayer;", at = @At("HEAD"))
-    public void teleportHead(TeleportTransition transition, CallbackInfoReturnable<ServerPlayer> callbackInfo) {
+    public void teleportHead(TeleportTransition transition, CallbackInfoReturnable<ServerPlayer> callbackInfo, @Share("fromDimHolder") LocalRef<ResourceKey<Level>> fromDimHolder) {
         ServerPlayer player = (ServerPlayer) (Object) this;
         fromDimHolder.set(player.serverLevel().dimension());
     }
 
     @Inject(method = "teleport(Lnet/minecraft/world/level/portal/TeleportTransition;)Lnet/minecraft/server/level/ServerPlayer;", at = @At("RETURN"))
-    public void teleportTail(TeleportTransition transition, CallbackInfoReturnable<ServerPlayer> callbackInfo) {
+    public void teleportTail(TeleportTransition transition, CallbackInfoReturnable<ServerPlayer> callbackInfo, @Share("fromDimHolder") LocalRef<ResourceKey<Level>> fromDimHolder) {
         ServerPlayer player = (ServerPlayer) (Object) this;
         final ResourceKey<Level> fromDim = fromDimHolder.get();
         final ResourceKey<Level> toDim = transition.newLevel().dimension();
-        if(!fromDim.equals(toDim)) {
+        if (!fromDim.equals(toDim)) {
             Balm.getEvents().fireEvent(new PlayerChangedDimensionEvent(player, fromDim, toDim));
         }
     }
