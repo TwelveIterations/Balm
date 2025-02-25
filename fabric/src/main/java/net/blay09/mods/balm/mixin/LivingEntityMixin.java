ecdae055a4e91481b7335542ce2f00eb3778fa90
Ampflower <ampflower@outlook.com>
2025-02-25 01:26:18 -0800

    fix: Use MixinExtras over ThreadLocal (#130)
    * Fix: Always clear fall event
    
    If the event hasn't been modified, this can temporarily leak an entity and its world, causing memory usage to inflate on the client.
    
    * Port fall event to MixinExtras
    
    This removes the need to store a global variable.
    
    * Use Share local for fromDimHolder
    
    * Use Share local for balmCurrentUseEvent
    
    This could've ideally just had `@Cancellable CallbackInfo` but it appears that this has an older Mixin Extras.
    
    * Use Share local for getGrowthSpeedBlockState, renamed to farmBlock

--- fabric/src/main/java/net/blay09/mods/balm/mixin/LivingEntityMixin.java
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/LivingEntityMixin.java
@@ -1,5 +1,9 @@
 package net.blay09.mods.balm.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.event.LivingDamageEvent;
 import net.blay09.mods.balm.api.event.LivingFallEvent;
@@ -8,7 +12,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -16,9 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-
-    @Unique
-    private static final ThreadLocal<LivingFallEvent> balmCurrentFallEvent = new ThreadLocal<>();
 
     @ModifyVariable(method = "actuallyHurt(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;setAbsorptionAmount(F)V"), argsOnly = true)
     private float actuallyHurt(float damageAmount, ServerLevel serverLevel, DamageSource damageSource) {
@@ -32,22 +32,22 @@ public abstract class LivingEntityMixin {
     }
 
     @Inject(method = "causeFallDamage(FFLnet/minecraft/world/damagesource/DamageSource;)Z", at = @At("HEAD"), cancellable = true)
-    private void causeFallDamage(float distance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> callbackInfo) {
+    private void causeFallDamage(float distance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> callbackInfo, @Share("eventRef") LocalRef<LivingFallEvent> eventRef) {
         LivingFallEvent event = new LivingFallEvent((LivingEntity) (Object) this);
         Balm.getEvents().fireEvent(event);
         if (event.isCanceled()) {
             callbackInfo.setReturnValue(false);
         }
-        balmCurrentFallEvent.set(event);
+        eventRef.set(event);
     }
 
-    @Inject(method = "calculateFallDamage(FF)I", at = @At("RETURN"), cancellable = true)
-    private void calculateFallDamage(float f, float g, CallbackInfoReturnable<Integer> cir) {
-        LivingFallEvent event = balmCurrentFallEvent.get();
+    @WrapOperation(method = "causeFallDamage(FFLnet/minecraft/world/damagesource/DamageSource;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;calculateFallDamage(FF)I"))
+    private int calculateFallDamage(LivingEntity self, float fallDistance, float multiplier, Operation<Integer> operation, @Share("eventRef") LocalRef<LivingFallEvent> eventRef) {
+        LivingFallEvent event = eventRef.get();
         if (event != null && event.getFallDamageOverride() != null) {
-            cir.setReturnValue(event.getFallDamageOverride().intValue());
-            balmCurrentFallEvent.set(null);
+            return event.getFallDamageOverride().intValue();
         }
+        return operation.call(self, fallDistance, multiplier);
     }
 
     @ModifyVariable(method = "heal(F)V", at = @At("HEAD"), argsOnly = true)
