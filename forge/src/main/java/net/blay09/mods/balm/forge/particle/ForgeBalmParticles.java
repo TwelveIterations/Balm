21246e58d3199c1ef50a401b77a74bf35e3e3226
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-27 09:50:13 +0100

    feat: Add BalmParticles.createSimple to allow access to SimpleParticleType

--- forge/src/main/java/net/blay09/mods/balm/forge/particle/ForgeBalmParticles.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/particle/ForgeBalmParticles.java
@@ -5,6 +5,7 @@ import net.blay09.mods.balm.api.particle.BalmParticles;
 import net.blay09.mods.balm.forge.DeferredRegisters;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -16,5 +17,10 @@ public class ForgeBalmParticles implements BalmParticles {
         final var register = DeferredRegisters.get(ForgeRegistries.PARTICLE_TYPES, identifier.getNamespace());
         final var registryObject = register.register(identifier.getPath(), () -> supplier.apply(identifier));
         return new DeferredObject<>(identifier, registryObject, registryObject::isPresent);
+    }
+
+    @Override
+    public SimpleParticleType createSimple(boolean overrideLimiter) {
+        return new SimpleParticleType(overrideLimiter);
     }
 }
