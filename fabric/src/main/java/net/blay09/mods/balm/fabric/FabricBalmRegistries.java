a56f342252cc4fa7da9146314e0adf76bed31914
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-27 09:40:01 +0100

    feat: Add untyped register function to cover all registries

--- fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalmRegistries.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalmRegistries.java
@@ -1,7 +1,12 @@
 package net.blay09.mods.balm.fabric;
 
 import net.blay09.mods.balm.api.BalmRegistries;
+import net.blay09.mods.balm.api.DeferredObject;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluid;
+
+import java.util.function.Function;
 
 public class FabricBalmRegistries implements BalmRegistries {
     public Fluid milkFluid;
@@ -14,5 +19,11 @@ public class FabricBalmRegistries implements BalmRegistries {
     @Override
     public Fluid getMilkFluid() {
         return milkFluid;
+    }
+
+    @Override
+    public <T> DeferredObject<T> register(Registry<T> registry, Function<ResourceLocation, T> supplier, ResourceLocation identifier) {
+        return new DeferredObject<>(identifier,
+                () -> Registry.register(registry, identifier, supplier.apply(identifier))).resolveImmediately();
     }
 }
