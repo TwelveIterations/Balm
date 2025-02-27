ad4b3ff6ea2124bffd2eac849a400ebf98d03944
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-27 14:08:56 +0100

    feat: Add BalmRenderers.registerParticleProvider

--- common/src/main/java/net/blay09/mods/balm/api/client/rendering/BalmRenderers.java
+++ common/src/main/java/net/blay09/mods/balm/api/client/rendering/BalmRenderers.java
@@ -4,9 +4,14 @@ import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.particle.ParticleEngine;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -15,6 +20,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public interface BalmRenderers {
@@ -32,4 +38,8 @@ public interface BalmRenderers {
      */
     @Deprecated
     void setBlockRenderType(Supplier<Block> block, RenderType renderType);
+
+    <T extends ParticleOptions> void registerParticleProvider(Supplier<ParticleType<T>> particleType, Function<SpriteSet, ParticleProvider<T>> factory);
+
+    <T extends ParticleOptions> void registerParticleProvider(Supplier<ParticleType<T>> particleType, ParticleProvider<T> provider);
 }
--- forge/src/main/java/net/blay09/mods/balm/forge/client/rendering/ForgeBalmRenderers.java
+++ forge/src/main/java/net/blay09/mods/balm/forge/client/rendering/ForgeBalmRenderers.java
@@ -6,10 +6,13 @@ import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -19,6 +22,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -29,6 +33,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ForgeBalmRenderers implements BalmRenderers {
@@ -51,12 +56,21 @@ public class ForgeBalmRenderers implements BalmRenderers {
         }
     }
 
+    private record ParticleProviderFactoryRegistration<T extends ParticleOptions>(Supplier<ParticleType<T>> particleType,
+                                                                                  Function<SpriteSet, ParticleProvider<T>> value) {
+    }
+
+    private record ParticleProviderRegistration<T extends ParticleOptions>(Supplier<ParticleType<T>> particleType, ParticleProvider<T> value) {
+    }
+
     private static class Registrations {
         public final Map<ModelLayerLocation, Supplier<LayerDefinition>> layerDefinitions = new HashMap<>();
         public final List<Pair<Supplier<BlockEntityType<?>>, BlockEntityRendererProvider<BlockEntity>>> blockEntityRenderers = new ArrayList<>();
         public final List<Pair<Supplier<EntityType<?>>, EntityRendererProvider<Entity>>> entityRenderers = new ArrayList<>();
         public final List<ColorRegistration<BlockColor, Block>> blockColors = new ArrayList<>();
         public final List<ColorRegistration<ItemColor, ItemLike>> itemColors = new ArrayList<>();
+        public final List<ParticleProviderFactoryRegistration<?>> particleProviderFactories = new ArrayList<>();
+        public final List<ParticleProviderRegistration<?>> particleProviders = new ArrayList<>();
 
         @SubscribeEvent
         public void setupClient(FMLClientSetupEvent event) {
@@ -92,6 +106,24 @@ public class ForgeBalmRenderers implements BalmRenderers {
             for (ColorRegistration<ItemColor, ItemLike> itemColor : itemColors) {
                 event.register(itemColor.getColor(), itemColor.getObjects().get());
             }
+        }
+
+        @SubscribeEvent
+        public void initParticleProviders(RegisterParticleProvidersEvent event) {
+            for (final var factory : particleProviderFactories) {
+                registerParticleProviderFactory(event, factory);
+            }
+            for (final var provider : particleProviders) {
+                registerParticleProvider(event, provider);
+            }
+        }
+
+        private <T extends ParticleOptions> void registerParticleProviderFactory(RegisterParticleProvidersEvent event, ParticleProviderFactoryRegistration<T> registration) {
+            event.registerSpriteSet(registration.particleType.get(), spriteSet -> registration.value().apply(spriteSet));
+        }
+
+        private <T extends ParticleOptions> void registerParticleProvider(RegisterParticleProvidersEvent event, ParticleProviderRegistration<T> registration) {
+            event.registerSpriteSet(registration.particleType.get(), spriteSet -> registration.value());
         }
     }
 
@@ -130,6 +162,16 @@ public class ForgeBalmRenderers implements BalmRenderers {
     public void setBlockRenderType(Supplier<Block> block, RenderType renderType) {
         // Do nothing in Forge. Forge unfortunately changes the Vanilla model format,
         // so we have to have both this call (for Fabric) and change the JSON (for Forge).
+    }
+
+    @Override
+    public <T extends ParticleOptions> void registerParticleProvider(Supplier<ParticleType<T>> particleType, Function<SpriteSet, ParticleProvider<T>> factory) {
+        getActiveRegistrations().particleProviderFactories.add(new ParticleProviderFactoryRegistration<>(particleType, factory));
+    }
+
+    @Override
+    public <T extends ParticleOptions> void registerParticleProvider(Supplier<ParticleType<T>> particleType, ParticleProvider<T> provider) {
+        getActiveRegistrations().particleProviders.add(new ParticleProviderRegistration<>(particleType, provider));
     }
 
     public void register() {
