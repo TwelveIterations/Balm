7ffacb4b121594f2470b24830abacccaa41d2247
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-05 17:42:58 +0100

    fix: Fix OnLoadHandler not firing when block entities are loaded from disk

--- fabric/src/main/java/net/blay09/mods/balm/mixin/LevelChunkMixin.java
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/LevelChunkMixin.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.balm.mixin;
 
 import net.blay09.mods.balm.api.block.entity.OnLoadHandler;
+import net.blay09.mods.balm.fabric.block.entity.BlockEntityOnLoadCallback;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.chunk.LevelChunk;
@@ -9,6 +10,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(LevelChunk.class)
 public class LevelChunkMixin {
@@ -19,10 +22,16 @@ public class LevelChunkMixin {
     @Shadow
     private Level level;
 
+    @Inject(method = "registerAllBlockEntitiesAfterLevelLoad", at = @At("HEAD"))
+    private void registerAllBlockEntitiesAfterLevelLoad(CallbackInfo callbackInfo) {
+        BlockEntityOnLoadCallback.scheduleOnLoad(((LevelChunk) (Object) this).getBlockEntities().values());
+    }
+
     @Inject(method = "addAndRegisterBlockEntity(Lnet/minecraft/world/level/block/entity/BlockEntity;)V", at = @At("RETURN"))
     private void addAndRegisterBlockEntity(BlockEntity blockEntity, CallbackInfo callbackInfo) {
-        if ((this.loaded || this.level.isClientSide()) && blockEntity instanceof OnLoadHandler) {
-            ((OnLoadHandler) blockEntity).onLoad();
+        if ((this.loaded || this.level.isClientSide()) && blockEntity instanceof OnLoadHandler handler) {
+            BlockEntityOnLoadCallback.scheduleOnLoad(List.of(blockEntity));
         }
     }
+
 }
---
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/LevelMixin.java
@@ -0,0 +1,17 @@
+package net.blay09.mods.balm.mixin;
+
+import net.blay09.mods.balm.fabric.block.entity.BlockEntityOnLoadCallback;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Level.class)
+public class LevelMixin {
+
+    @Inject(method = "tickBlockEntities", at = @At("HEAD"))
+    private void tickBlockEntities(CallbackInfo callbackInfo) {
+        BlockEntityOnLoadCallback.fireOnLoad();
+    }
+}
