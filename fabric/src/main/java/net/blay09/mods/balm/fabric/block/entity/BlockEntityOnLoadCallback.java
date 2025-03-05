12dcff645fe5dd90f8b6ec7ada131ff6b5519a1b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-05 17:42:58 +0100

    fix: Fix OnLoadHandler not firing when block entities are loaded from disk

---
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/block/entity/BlockEntityOnLoadCallback.java
@@ -0,0 +1,36 @@
+package net.blay09.mods.balm.fabric.block.entity;
+
+import net.blay09.mods.balm.api.block.entity.OnLoadHandler;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BlockEntityOnLoadCallback {
+
+    private static final List<BlockEntity> pendingFreshBlockEntities = new ArrayList<>();
+    private static final List<BlockEntity> freshBlockEntities = new ArrayList<>();
+    private static boolean callbacksRunning = false;
+
+    public static void scheduleOnLoad(Collection<BlockEntity> blockEntities) {
+        if (callbacksRunning) {
+            pendingFreshBlockEntities.addAll(blockEntities);
+        } else {
+            freshBlockEntities.addAll(blockEntities);
+        }
+    }
+
+    public static void fireOnLoad() {
+        freshBlockEntities.addAll(pendingFreshBlockEntities);
+        pendingFreshBlockEntities.clear();
+        callbacksRunning = true;
+        for (final var blockEntity : freshBlockEntities) {
+            if (blockEntity instanceof OnLoadHandler handler) {
+                handler.onLoad();
+            }
+        }
+        freshBlockEntities.clear();
+        callbacksRunning = false;
+    }
+}
