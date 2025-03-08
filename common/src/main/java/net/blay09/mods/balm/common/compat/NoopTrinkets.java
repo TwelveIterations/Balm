528b10ac26d5e2bbedad71810947ce8b598a90f3
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-08 08:41:13 +0100

    feat: Use mod proxy for trinkets support

--- common/src/main/java/net/blay09/mods/balm/common/compat/NoopTrinkets.java
+++ common/src/main/java/net/blay09/mods/balm/common/compat/NoopTrinkets.java
@@ -1,6 +1,5 @@
-package net.blay09.mods.balm.fabric.compat.trinkets;
+package net.blay09.mods.balm.common.compat;
 
-import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.compat.trinkets.BalmModSupportTrinkets;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -8,28 +7,19 @@ import net.minecraft.world.item.ItemStack;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class FabricBalmModSupportTrinkets implements BalmModSupportTrinkets {
+public class NoopTrinkets implements BalmModSupportTrinkets {
     @Override
     public boolean isEquipped(Player player, Predicate<ItemStack> predicate) {
-        if (Balm.isModLoaded("trinkets")) {
-            return TrinketsModCompat.isEquipped(player, predicate);
-        }
         return false;
     }
 
     @Override
     public ItemStack findEquipped(Player player, Predicate<ItemStack> predicate) {
-        if (Balm.isModLoaded("trinkets")) {
-            return TrinketsModCompat.findEquipped(player, predicate);
-        }
         return ItemStack.EMPTY;
     }
 
     @Override
     public List<ItemStack> findAllEquipped(Player player, Predicate<ItemStack> predicate) {
-        if (Balm.isModLoaded("trinkets")) {
-            return TrinketsModCompat.findAllEquipped(player, predicate);
-        }
         return List.of();
     }
 
