73e890ccae2890b2a76fded5321e7d72bac4282d
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-08 08:41:13 +0100

    feat: Use mod proxy for trinkets support

--- fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalmRuntime.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/FabricBalmRuntime.java
@@ -80,7 +80,7 @@ public class FabricBalmRuntime implements BalmRuntime {
     private final BalmLootTables lootTables = new CommonBalmLootTables();
     private final BalmStats stats = new FabricBalmStats();
     private final BalmRecipes recipes = new FabricBalmRecipes();
-    private final BalmModSupport modSupport = new FabricBalmModSupport();
+    private final BalmModSupport modSupport = new FabricBalmModSupport(this);
     private final BalmParticles particles = new FabricBalmParticles();
     private final BalmPermissions permissions = this.<BalmPermissions>modProxy()
             .with("fabric-permissions-api-v0", "net.blay09.mods.balm.fabric.compat.FabricPermissionsAPIIntegration")
