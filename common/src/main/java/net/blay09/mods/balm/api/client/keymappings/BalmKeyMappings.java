ae8f3fce46fa704f0dd28d186087ac8a84262b3f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-18 18:53:56 +0100

    fix: Remove key mapping mixins in favor of Kuma to avoid Essential loading Balm too early #135

--- common/src/main/java/net/blay09/mods/balm/api/client/keymappings/BalmKeyMappings.java
+++ common/src/main/java/net/blay09/mods/balm/api/client/keymappings/BalmKeyMappings.java
@@ -5,38 +5,66 @@ import net.minecraft.client.KeyMapping;
 
 import java.util.Optional;
 
+/**
+ * @deprecated Use Kuma instead.
+ */
+@Deprecated
 public interface BalmKeyMappings {
+    @Deprecated
     KeyMapping registerKeyMapping(String name, int keyCode, String category);
 
+    @Deprecated
     KeyMapping registerKeyMapping(String name, InputConstants.Type type, int keyCode, String category);
 
+    @Deprecated
     KeyMapping registerKeyMapping(String name, KeyConflictContext conflictContext, KeyModifier modifier, int keyCode, String category);
 
+    @Deprecated
     KeyMapping registerKeyMapping(String name, KeyConflictContext conflictContext, KeyModifiers modifiers, int keyCode, String category);
 
+    @Deprecated
     KeyMapping registerKeyMapping(String name, KeyConflictContext conflictContext, KeyModifier modifier, InputConstants.Type type, int keyCode, String category);
 
+    @Deprecated
     KeyMapping registerKeyMapping(String name, KeyConflictContext conflictContext, KeyModifiers modifiers, InputConstants.Type type, int keyCode, String category);
 
+    @Deprecated
     default boolean isActiveAndMatches(KeyMapping keyMapping, int keyCode, int scanCode) {
         return isActiveAndMatches(keyMapping, InputConstants.getKey(keyCode, scanCode));
     }
 
+    @Deprecated
     default boolean isActiveAndMatches(KeyMapping keyMapping, InputConstants.Type type, int keyCode, int scanCode) {
         return isActiveAndMatches(keyMapping, type.getOrCreate(type == InputConstants.Type.SCANCODE ? scanCode : keyCode));
     }
 
+    @Deprecated
     boolean isActiveAndMatches(KeyMapping keyMapping, InputConstants.Key input);
 
+    @Deprecated
     boolean isActiveAndWasPressed(KeyMapping keyMapping);
 
+    @Deprecated
     boolean isKeyDownIgnoreContext(KeyMapping keyMapping);
 
+    @Deprecated
     boolean isActiveAndKeyDown(KeyMapping keyMapping);
 
+    /**
+     * @deprecated No longer in use.
+     */
+    @Deprecated
     Optional<Boolean> conflictsWith(KeyMapping first, KeyMapping second);
 
+    /**
+     * @deprecated No longer in use. Has no effect.
+     */
+    @Deprecated
     void ignoreConflicts(KeyMapping keyMapping);
 
+    /**
+     * @deprecated No longer in use.
+     */
+    @Deprecated
     boolean shouldIgnoreConflicts(KeyMapping keyMapping);
 }
