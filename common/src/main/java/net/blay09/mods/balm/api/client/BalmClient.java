05b5dc619a0de5551696a975556589a154516bdd
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-22 09:14:57 +0100

    feat: Add Balm.registerModule and BalmClient.registerModule as a more structured and stable alternative to interact with Balm

--- common/src/main/java/net/blay09/mods/balm/api/client/BalmClient.java
+++ common/src/main/java/net/blay09/mods/balm/api/client/BalmClient.java
@@ -2,14 +2,25 @@ package net.blay09.mods.balm.api.client;
 
 import net.blay09.mods.balm.api.BalmRuntimeLoadContext;
 import net.blay09.mods.balm.api.client.keymappings.BalmKeyMappings;
+import net.blay09.mods.balm.api.client.module.BalmClientModule;
 import net.blay09.mods.balm.api.client.rendering.BalmModels;
 import net.blay09.mods.balm.api.client.rendering.BalmRenderers;
 import net.blay09.mods.balm.api.client.rendering.BalmTextures;
 import net.blay09.mods.balm.api.client.screen.BalmScreens;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BalmClient {
 
     private static final BalmClientRuntime<BalmRuntimeLoadContext> runtime = BalmClientRuntimeSpi.create();
+    private static final List<BalmClientModule> modules = Collections.synchronizedList(new ArrayList<>());
+
+    public static void registerModule(BalmClientModule module) {
+        modules.add(module);
+        runtime.initializeModule(module);
+    }
 
     public static <T extends BalmRuntimeLoadContext> void initialize(String modId, T context, Runnable initializer) {
         runtime.initialize(modId, context, initializer);
---
+++ common/src/main/java/net/blay09/mods/balm/api/client/module/BalmClientModule.java
@@ -0,0 +1,30 @@
+package net.blay09.mods.balm.api.client.module;
+
+import net.blay09.mods.balm.api.client.keymappings.BalmKeyMappings;
+import net.blay09.mods.balm.api.client.rendering.BalmModels;
+import net.blay09.mods.balm.api.client.rendering.BalmRenderers;
+import net.blay09.mods.balm.api.client.screen.BalmScreens;
+import net.blay09.mods.balm.api.event.BalmEvents;
+import net.minecraft.resources.ResourceLocation;
+
+public interface BalmClientModule {
+    ResourceLocation getId();
+
+    default void registerEvents(BalmEvents events) {
+    }
+
+    default void registerModels(BalmModels models) {
+    }
+
+    default void registerRenderers(BalmRenderers renderers) {
+    }
+
+    default void registerScreens(BalmScreens screens) {
+    }
+
+    default void registerKeyMappings(BalmKeyMappings keyMappings) {
+    }
+
+    default void initialize() {
+    }
+}
