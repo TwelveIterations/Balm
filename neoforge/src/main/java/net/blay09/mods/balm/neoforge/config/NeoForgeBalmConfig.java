f96066a7f67b70608012cb03258cda55180e0516
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-23 23:02:37 +0100

    wip: Update NeoForge to Minecraft 1.21.5

--- neoforge/src/main/java/net/blay09/mods/balm/neoforge/config/NeoForgeBalmConfig.java
+++ neoforge/src/main/java/net/blay09/mods/balm/neoforge/config/NeoForgeBalmConfig.java
@@ -2,6 +2,7 @@ package net.blay09.mods.balm.neoforge.config;
 
 import net.blay09.mods.balm.api.config.schema.BalmConfigSchema;
 import net.blay09.mods.balm.common.config.AbstractBalmConfig;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.loading.FMLPaths;
 
 import java.io.File;
@@ -17,7 +18,11 @@ public class NeoForgeBalmConfig extends AbstractBalmConfig {
     public void registerConfig(BalmConfigSchema schema) {
         super.registerConfig(schema);
         final var stringType = schema.identifier().getPath();
-
+        final var configType = switch (stringType) {
+            case "common" -> ModConfig.Type.COMMON;
+            case "client" -> ModConfig.Type.CLIENT;
+            default -> throw new IllegalArgumentException("Unsupported config type: " + stringType + " - only 'common' and 'client' are supported.");
+        };
         // TODO 1.21.5: Configs
     }
 }
