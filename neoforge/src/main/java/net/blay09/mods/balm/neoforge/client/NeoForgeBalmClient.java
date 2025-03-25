cc56958e3edc6ce17b6e5d29363d1fb6bfc6af9d
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-25 09:14:49 +0100

    fix(1.21.5): Fix client runtime not being initialized on NeoForge

--- neoforge/src/main/java/net/blay09/mods/balm/neoforge/client/NeoForgeBalmClient.java
+++ neoforge/src/main/java/net/blay09/mods/balm/neoforge/client/NeoForgeBalmClient.java
@@ -1,12 +1,12 @@
 package net.blay09.mods.balm.neoforge.client;
 
-import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.client.BalmClient;
-import net.blay09.mods.balm.api.event.client.DisconnectedFromServerEvent;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.Mod;
 
+@Mod(value = "balm", dist = Dist.CLIENT)
 public class NeoForgeBalmClient {
-    public static void onInitializeClient(FMLClientSetupEvent setupEvent) {
+    public NeoForgeBalmClient() {
         ((NeoForgeBalmClientRuntime) BalmClient.getRuntime()).initializeRuntime();
     }
 }
