6c40316977b361d7a17f401efc9187165956ce31
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-26 12:11:00 +0100

    fix: Improve type safety of resource condition registration #142

--- fabric/src/main/java/net/blay09/mods/balm/fabric/resources/FabricBalmResources.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/resources/FabricBalmResources.java
@@ -3,13 +3,9 @@ package net.blay09.mods.balm.fabric.resources;
 import com.mojang.serialization.MapCodec;
 import net.blay09.mods.balm.api.resources.BalmResourceCondition;
 import net.blay09.mods.balm.api.resources.BalmResources;
-import net.blay09.mods.balm.common.resources.ResourceConditionContextImpl;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceCondition;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditionType;
 import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
-import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,18 +14,10 @@ public class FabricBalmResources implements BalmResources {
     private final Map<ResourceLocation, ResourceConditionType<?>> conditions = new HashMap<>();
 
     @Override
-    public void registerResourceCondition(ResourceLocation identifier, MapCodec<BalmResourceCondition> codec) {
-        final var type = ResourceConditionType.create(identifier, codec.xmap(it -> new ResourceCondition() {
-            @Override
-            public ResourceConditionType<?> getType() {
-                return conditions.get(identifier);
-            }
-
-            @Override
-            public boolean test(@Nullable RegistryOps.RegistryInfoLookup registryInfoLookup) {
-                return it.test(new ResourceConditionContextImpl(registryInfoLookup));
-            }
-        }, it -> (BalmResourceCondition) context -> it.test((RegistryOps.RegistryInfoLookup) context.backingContext())));
+    public <T extends BalmResourceCondition> void registerResourceCondition(ResourceLocation identifier, MapCodec<T> codec) {
+        final var type = ResourceConditionType.create(identifier, codec
+                .xmap(it -> new FabricBalmResourceCondition<>(identifier, it, conditions::get),
+                        FabricBalmResourceCondition::delegate));
         ResourceConditions.register(type);
         conditions.put(identifier, type);
     }
---
+++ neoforge/src/main/java/net/blay09/mods/balm/neoforge/resources/NeoForgeBalmResourceCondition.java
@@ -0,0 +1,22 @@
+package net.blay09.mods.balm.neoforge.resources;
+
+import com.mojang.serialization.MapCodec;
+import net.blay09.mods.balm.api.resources.BalmResourceCondition;
+import net.blay09.mods.balm.common.resources.ResourceConditionContextImpl;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.conditions.ICondition;
+
+import java.util.function.Function;
+
+public record NeoForgeBalmResourceCondition<T extends BalmResourceCondition>(ResourceLocation identifier, T delegate,
+                                                                             Function<ResourceLocation, MapCodec<? extends ICondition>> codecResolver) implements ICondition {
+    @Override
+    public boolean test(IContext context) {
+        return delegate.test(new ResourceConditionContextImpl(context));
+    }
+
+    @Override
+    public MapCodec<? extends ICondition> codec() {
+        return codecResolver.apply(identifier);
+    }
+}
