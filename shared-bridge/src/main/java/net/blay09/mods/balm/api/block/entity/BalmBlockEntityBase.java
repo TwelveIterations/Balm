5fcdfc7d9f356714fa9c41aad2fe66e3a28b4650
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-29 19:51:13 +0100

    fix: Fix compile error due to javadoc

--- shared-bridge/src/main/java/net/blay09/mods/balm/api/block/entity/BalmBlockEntityBase.java
+++ shared-bridge/src/main/java/net/blay09/mods/balm/api/block/entity/BalmBlockEntityBase.java
@@ -1,15 +1,11 @@
 package net.blay09.mods.balm.api.block.entity;
 
-import net.blay09.mods.balm.api.capability.CapabilityType;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
 
 public abstract class BalmBlockEntityBase extends BlockEntity {
     public BalmBlockEntityBase(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
@@ -17,7 +13,7 @@ public abstract class BalmBlockEntityBase extends BlockEntity {
     }
 
     /**
-     * @deprecated Use {@link net.blay09.mods.balm.api.capability.BalmCapabilities#registerProvider(ResourceLocation, CapabilityType, BiFunction, Supplier)} instead.
+     * @deprecated Use <code>BalmCapabilities#registerProvider(ResourceLocation, CapabilityType, BiFunction, Supplier)</code> instead.
      */
     @Deprecated(forRemoval = true, since = "1.21.5")
     protected abstract void buildProviders(List<Object> providers);
