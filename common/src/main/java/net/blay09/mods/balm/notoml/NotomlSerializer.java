71c831097db42abe1ea6c64284c0aa7eba81312a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-04-11 08:18:03 +0200

    fix: Don't write empty comments in toml #162

--- common/src/main/java/net/blay09/mods/balm/notoml/NotomlSerializer.java
+++ common/src/main/java/net/blay09/mods/balm/notoml/NotomlSerializer.java
@@ -19,7 +19,7 @@ public class NotomlSerializer {
         var sortedCategoryKeys = data.rowKeySet().stream().sorted().toList();
         for (String category : sortedCategoryKeys) {
             String categoryComment = comments.get(category, "");
-            if (categoryComment != null) {
+            if (categoryComment != null && !categoryComment.isEmpty()) {
                 sb.append("\n").append("# ").append(categoryComment).append("\n");
             }
 
@@ -31,7 +31,7 @@ public class NotomlSerializer {
             var sortedPropertyKeys = categoryProperties.keySet().stream().sorted().toList();
             for (String property : sortedPropertyKeys) {
                 String propertyComment = comments.get(category, property);
-                if (propertyComment != null) {
+                if (propertyComment != null && !propertyComment.isEmpty()) {
                     sb.append("\n").append("# ").append(propertyComment).append("\n");
                 }
 
