f3e3abac1432893799557121413cad39b3bec3ff
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-05-29 10:20:01 +0200

    feat(1.21.6): Add de/serialize with ValueInput/Output to DefaultEnergyStorage

--- common/src/main/java/net/blay09/mods/balm/api/energy/DefaultEnergyStorage.java
+++ common/src/main/java/net/blay09/mods/balm/api/energy/DefaultEnergyStorage.java
@@ -1,7 +1,12 @@
 package net.blay09.mods.balm.api.energy;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.IntTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 public class DefaultEnergyStorage implements EnergyStorage {
 
@@ -76,10 +81,26 @@ public class DefaultEnergyStorage implements EnergyStorage {
         return maxFill > 0;
     }
 
+    public void serialize(ValueOutput output) {
+        output.putInt("Energy", energy);
+    }
+
+    public void deserialize(ValueInput input) {
+        energy = input.getIntOr("Energy", 0);
+    }
+
+    /**
+     * @deprecated Use {@link #serialize(ValueOutput)} instead. Note that the data format now uses an "Energy" key instead of returning an IntTag directly.
+     */
+    @Deprecated(forRemoval = true, since = "1.22")
     public IntTag serialize() {
         return IntTag.valueOf(energy);
     }
 
+    /**
+     * @deprecated Use {@link #deserialize(ValueInput)} instead. Note that the data format now uses an "Energy" key instead of returning an IntTag directly.
+     */
+    @Deprecated(forRemoval = true, since = "1.22")
     public void deserialize(Tag tag) {
         energy = ((IntTag) tag).intValue();
     }
