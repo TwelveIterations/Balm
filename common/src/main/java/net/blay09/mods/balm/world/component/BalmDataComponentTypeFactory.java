9011cdca0267f40ad71938f8e0476975b783f350
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-03 14:01:08 +0100

    feat: Add BalmDataComponentTypeFactory#register overloads that take a codec directly for the most common use cases

--- common/src/main/java/net/blay09/mods/balm/world/component/BalmDataComponentTypeFactory.java
+++ common/src/main/java/net/blay09/mods/balm/world/component/BalmDataComponentTypeFactory.java
@@ -1,6 +1,9 @@
 package net.blay09.mods.balm.world.component;
 
+import com.mojang.serialization.Codec;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.BiFunction;
@@ -10,6 +13,14 @@ import java.util.function.Function;
  * Provides convenience access to registering data component types.
  */
 public interface BalmDataComponentTypeFactory {
+
+    default <T> BalmDataComponentTypeRegistration<T> register(String name, Codec<T> codec) {
+        return register(name, (id, builder) -> this.<T>createBuilder().persistent(codec));
+    }
+
+    default <T> BalmDataComponentTypeRegistration<T> register(String name, Codec<T> codec, StreamCodec<? super RegistryFriendlyByteBuf, T> streamCodec) {
+        return register(name, (id, builder) -> this.<T>createBuilder().persistent(codec).networkSynchronized(streamCodec));
+    }
 
     default <T> BalmDataComponentTypeRegistration<T> register(String name, Function<DataComponentType.Builder<T>, DataComponentType.Builder<T>> builderConsumer) {
         return register(name, (id, builder) -> builderConsumer.apply(builder));
