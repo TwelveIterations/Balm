034e361823ac82b2142b15cb6d434b22e060ba20
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-03 10:07:22 +0100

    docs: Add javadoc for some Balm methods

--- common/src/main/java/net/blay09/mods/balm/api/Balm.java
+++ common/src/main/java/net/blay09/mods/balm/api/Balm.java
@@ -57,14 +57,47 @@ public class Balm {
         runtime.onRuntimeAvailable(callback);
     }
 
+    /**
+     * You must call this or any of its overloads in each of your mod's entry points. Provide a load context specific to each mod loader.
+     * Everything else you do in Balm should happen inside the initializer, which runs at a time that Balm has
+     * initialized the runtime for your mod.
+     *
+     * @param modId       The mod id for your mod.
+     * @param context     The load context for the mod loader you are using, e.g. NeoForgeLoadContext.
+     * @param initializer Callback that runs when Balm is ready, at which point you can use its methods to set up your mod.
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, BalmModule)
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, BalmModule...)
+     */
     public static void initializeMod(String modId, BalmRuntimeLoadContext context, Runnable initializer) {
         runtime.initializeMod(modId, context, initializer);
     }
 
+    /**
+     * You must call this or any of its overloads in each of your mod's entry points. Provide a load context specific to each mod loader.
+     * Everything else you do in Balm should happen inside the module, whose methods are called at a time that Balm has
+     * initialized the runtime for your mod.
+     *
+     * @param modId   the mod id for your mod.
+     * @param context the load context for the mod loader you are using, e.g. NeoForgeLoadContext.
+     * @param module  an implementation of {@link BalmModule} within which you can set up your mod.
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, Runnable)
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, BalmModule...)
+     */
     public static <T extends BalmRuntimeLoadContext> void initializeMod(String modId, T context, BalmModule module) {
         runtime.initializeMod(modId, context, () -> registerModule(module));
     }
 
+    /**
+     * You must call this or any of its overloads in each of your mod's entry points. Provide a load context specific to each mod loader.
+     * Everything else you do in Balm should happen inside the module, whose methods are called at a time that Balm has
+     * initialized the runtime for your mod.
+     *
+     * @param modId   the mod id for your mod.
+     * @param context the load context for the mod loader you are using, e.g. NeoForgeLoadContext.
+     * @param modules one or more implementations of {@link BalmModule} within which you can set up your mod.
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, Runnable)
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, BalmModule)
+     */
     public static <T extends BalmRuntimeLoadContext> void initializeMod(String modId, T context, BalmModule... modules) {
         runtime.initializeMod(modId, context, () -> {
             for (final var module : modules) {
@@ -73,18 +106,49 @@ public class Balm {
         });
     }
 
+    /**
+     * Creates a platform proxy, which you can use to provide differing implementations for each mod loader under a common interface.
+     *
+     * @param <T> the type of the common interface.
+     * @return a builder for a platform proxy.
+     */
     public static <T> PlatformProxy<T> platformProxy() {
         return runtime.platformProxy();
     }
 
+    /**
+     * Creates a mod proxy, which you can use to provide differing implementations depending on which mods are loaded under a common interface.
+     *
+     * @param <T> the type of the common interface.
+     * @return a builder for a mod proxy.
+     */
     public static <T> ModProxy<T> modProxy() {
         return runtime.modProxy();
     }
 
+    /**
+     * Creates a sided proxy, which you can use to provide differing implementations for whether the mod is running on the client or a dedicated server under a common interface.
+     * When providing class names, do not use {@link Class#getName()} as that will cause the class to be loaded and cause crashes on dedicated servers!
+     * Provide a literal string for the class names instead.
+     * Both classes must have a public no-arg constructor.
+     *
+     * @param commonName the class name to load on dedicated servers.
+     * @param clientName the class name to load on clients.
+     * @param <T>        the type of the common interface.
+     * @return a builder for a sided proxy.
+     */
     public static <T> SidedProxy<T> sidedProxy(String commonName, String clientName) {
         return runtime.sidedProxy(commonName, clientName);
     }
 
+    /**
+     * Instantiates the given class if the mod id in question is loaded.
+     * Do not use {@link Class#getName()} as that will cause the class to be loaded and cause crashes if the mod is not loaded!
+     * Provide a literal string for the class name instead. The class must have a public no-arg constructor.
+     *
+     * @param modId     the mod id that needs to be loaded for the class to be instantiated.
+     * @param className the name of the class to be instantiated.
+     */
     public static void initializeIfLoaded(String modId, String className) {
         runtime.initializeIfLoaded(modId, className);
     }
@@ -149,6 +213,14 @@ public class Balm {
         return runtime.getStats();
     }
 
+    /**
+     * Provides access to mod support related functions, such as
+     * <li>a universal milk fluid</li>
+     * <li>accessories (Curios/Trinkets)</li>
+     * <li>hud info mods (WAILA, Jade)</li>
+     *
+     * @return implementation of {@link BalmModSupport} for the environment Balm is running on.
+     */
     public static BalmModSupport modSupport() {
         return runtime.getModSupport();
     }
@@ -157,10 +229,20 @@ public class Balm {
         return runtime.getParticles();
     }
 
+    /**
+     * Provides access to registering and checking for permissions.
+     *
+     * @return implementation of {@link BalmPermissions} for the mod loader Balm is running on.
+     */
     public static BalmPermissions permissions() {
         return runtime.getPermissions();
     }
 
+    /**
+     * Provides access to mod jar file contents, resource load conditions, and resource reload listeners.
+     *
+     * @return implementation of {@link BalmResources} for the mod loader Balm is running on.
+     */
     public static BalmResources resources() {
         return runtime.getResources();
     }
@@ -174,39 +256,95 @@ public class Balm {
         return runtime;
     }
 
+    /**
+     * Use this to register blocks using the factory provided in the consumer callback.
+     *
+     * @param namespace   The mod id to register blocks under.
+     * @param initializer Callback that receives a scoped factory for registering blocks.
+     */
     public static void blocks(String namespace, Consumer<BalmBlockFactory> initializer) {
         runtime.blocks(namespace, initializer);
     }
 
+    /**
+     * Use this to register items using the factory provided in the consumer callback.
+     *
+     * @param namespace   The mod id to register items under.
+     * @param initializer Callback that receives a scoped factory for registering items.
+     */
     public static void items(String namespace, Consumer<BalmItemFactory> initializer) {
         runtime.items(namespace, initializer);
     }
 
+    /**
+     * Use this to register recipe types and related objects using the factory provided in the consumer callback.
+     *
+     * @param namespace   The mod id to register recipe types under.
+     * @param initializer Callback that receives a scoped factory for registering recipe types.
+     */
     public static void recipeTypes(String namespace, Consumer<BalmRecipeTypeFactory> initializer) {
         runtime.recipeTypes(namespace, initializer);
     }
 
+    /**
+     * Use this to register creative mode tabs using the factory provided in the consumer callback.
+     *
+     * @param namespace   The mod id to register creative mode tabs under.
+     * @param initializer Callback that receives a scoped factory for registering creative mode tabs.
+     */
     public static void creativeModeTabs(String namespace, Consumer<BalmCreativeModeTabFactory> initializer) {
         runtime.creativeModeTabs(namespace, initializer);
     }
 
+    /**
+     * Use this to register block entity types using the factory provided in the consumer callback.
+     *
+     * @param namespace   The mod id to register block entity types under.
+     * @param initializer Callback that receives a scoped factory for registering block entity types.
+     */
     public static void blockEntityTypes(String namespace, Consumer<BalmBlockEntityTypeFactory> initializer) {
         runtime.blockEntityTypes(namespace, initializer);
     }
 
+    /**
+     * Provides a generic registrar that can be used to register entries to any registry. Consider using a scoped registar instead.
+     *
+     * @return a {@link BalmRegistrar} that can be used to register entries to any registry.
+     * @see Balm#registrar(ResourceKey, String)
+     */
     public static BalmRegistrar registrar() {
         return runtime.registrar();
     }
 
+    /**
+     * Use this to register registry objects that are not covered by the convenient factories.
+     * Creates a scoped registrar for a specific registry and namespace.
+     *
+     * @param registryKey The {@link net.minecraft.core.registries.Registries} registry resource key.
+     * @param namespace   The mod id to register entries under.
+     * @param <T>         The type of the registry entries, e.g. {@link net.minecraft.sounds.SoundEvent}.
+     * @return a scoped {@link BalmRegistrar} that can be used to register entries to this registry.
+     * @see Balm#blocks(String, Consumer)
+     * @see Balm#blockEntityTypes(String, Consumer)
+     * @see Balm#items(String, Consumer)
+     * @see Balm#creativeModeTabs(String, Consumer)
+     * @see Balm#recipeTypes(String, Consumer)
+     */
     public static <T> BalmRegistrar.Scoped<T> registrar(ResourceKey<? extends Registry<T>> registryKey, String namespace) {
         return runtime.registrar(registryKey, namespace);
     }
 
+    /**
+     * Provides access to mod loader related functions, such as checking if a mod is loaded.
+     *
+     * @return implementation of {@link BalmPlatform} for the mod loader Balm is running on.
+     */
     public static BalmPlatform platform() {
         return runtime.platform();
     }
 
     /**
+     * @see Balm#permissions()
      * @deprecated Use {@link Balm#permissions()} instead.
      */
     @Deprecated
@@ -215,6 +353,7 @@ public class Balm {
     }
 
     /**
+     * @see Balm#modSupport()
      * @deprecated Use {@link Balm#modSupport()} instead.
      */
     @Deprecated
@@ -223,6 +362,9 @@ public class Balm {
     }
 
     /**
+     * @see Balm#registrar()
+     * @see Balm#registrar(ResourceKey, String)
+     * @see Balm#getModSupport()
      * @deprecated Use {@link Balm#registrar()} instead, or {@link Balm#getModSupport()} for the milk fluid.
      */
     @Deprecated
@@ -231,6 +373,7 @@ public class Balm {
     }
 
     /**
+     * @see Balm#blockEntityTypes(String, Consumer)
      * @deprecated Use {@link Balm#blockEntityTypes(String, Consumer)} instead.
      */
     @Deprecated
@@ -239,6 +382,7 @@ public class Balm {
     }
 
     /**
+     * @see Balm#items(String, Consumer)
      * @deprecated Use {@link Balm#items(String, Consumer)} instead.
      */
     @Deprecated
@@ -247,6 +391,7 @@ public class Balm {
     }
 
     /**
+     * @see Balm#blocks(String, Consumer)
      * @deprecated Use {@link Balm#blocks(String, Consumer)} instead.
      */
     @Deprecated
@@ -255,6 +400,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#platform()
+     * @see BalmPlatform#name()
      * @deprecated Use {@link Balm#platform()} and {@link BalmPlatform#name()} instead.
      */
     @Deprecated
@@ -263,6 +410,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#platform()
+     * @see BalmPlatform#physicalSide()
      * @deprecated Use {@link Balm#platform()} and {@link BalmPlatform#physicalSide()} instead.
      */
     @Deprecated
@@ -271,6 +420,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#platform()
+     * @see BalmPlatform#loadedPrimaryModIds()
      * @deprecated Use {@link Balm#platform()} and {@link BalmPlatform#loadedPrimaryModIds()} instead.
      */
     @Deprecated
@@ -279,6 +430,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#platform()
+     * @see BalmPlatform#isModLoaded(String)
      * @deprecated Use {@link Balm#platform()} and {@link BalmPlatform#isModLoaded(String)} instead.
      */
     @Deprecated
@@ -287,6 +440,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#platform()
+     * @see BalmPlatform#getModName(String)
      * @deprecated Use {@link Balm#platform()} and {@link BalmPlatform#getModName(String)} instead.
      */
     @Deprecated
@@ -295,6 +450,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#platform()
+     * @see BalmPlatform#isDevelopmentEnvironment()
      * @deprecated Use {@link Balm#platform()} and {@link BalmPlatform#isDevelopmentEnvironment()} instead.
      */
     @Deprecated
@@ -303,7 +460,8 @@ public class Balm {
     }
 
     /**
-     * @deprecated Use {@link net.blay09.mods.balm.api.Balm#registrar(net.minecraft.resources.ResourceKey, String)} instead.
+     * @see Balm#registrar(net.minecraft.resources.ResourceKey, String)
+     * @deprecated Use {@link Balm#registrar(net.minecraft.resources.ResourceKey, String)} instead.
      */
     @Deprecated
     public static BalmSounds getSounds() {
@@ -311,7 +469,8 @@ public class Balm {
     }
 
     /**
-     * @deprecated Use {@link net.blay09.mods.balm.api.Balm#registrar(net.minecraft.resources.ResourceKey, String)} instead.
+     * @see Balm#registrar(net.minecraft.resources.ResourceKey, String)
+     * @deprecated Use {@link Balm#registrar(net.minecraft.resources.ResourceKey, String)} instead.
      */
     @Deprecated
     public static BalmComponents getComponents() {
@@ -319,6 +478,7 @@ public class Balm {
     }
 
     /**
+     * @see Balm#resources()
      * @deprecated Use {@link Balm#resources()} instead.
      */
     @Deprecated
@@ -327,6 +487,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#resources()
+     * @see BalmResources#visitModResources(String, String, ModResourceVisitor)
      * @deprecated Use {@link Balm#resources()} and {@link BalmResources#visitModResources(String, String, ModResourceVisitor)} instead.
      */
     @Deprecated
@@ -335,6 +497,8 @@ public class Balm {
     }
 
     /**
+     * @see Balm#resources()
+     * @see BalmResources#lookupModResource(String, String)
      * @deprecated Use {@link Balm#resources()} and {@link BalmResources#lookupModResource(String, String)} instead.
      */
     @Deprecated
@@ -343,6 +507,7 @@ public class Balm {
     }
 
     /**
+     * @see #recipeTypes(String, java.util.function.Consumer)
      * @deprecated Use {@link #recipeTypes(String, java.util.function.Consumer)} to register recipe types and serializers.
      */
     @Deprecated
