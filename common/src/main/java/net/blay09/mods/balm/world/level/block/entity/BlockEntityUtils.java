309986564c89110d31d6bc07c58ef35b48dac0f9
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-05 18:28:57 +0100

    feat: Add BlockEntityUtils#createUpdateTag

--- common/src/main/java/net/blay09/mods/balm/world/level/block/entity/BlockEntityUtils.java
+++ common/src/main/java/net/blay09/mods/balm/world/level/block/entity/BlockEntityUtils.java
@@ -1,10 +1,16 @@
 package net.blay09.mods.balm.world.level.block.entity;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.storage.TagValueOutput;
+import net.minecraft.world.level.storage.ValueOutput;
+
+import java.util.function.Consumer;
 
 public class BlockEntityUtils {
     public static void sync(BlockEntity blockEntity) {
@@ -16,5 +22,11 @@ public class BlockEntityUtils {
 
     public static Packet<ClientGamePacketListener> createUpdatePacket(BlockEntity blockEntity) {
         return ClientboundBlockEntityDataPacket.create(blockEntity, BlockEntity::getUpdateTag);
+    }
+
+    public CompoundTag createUpdateTag(BlockEntity blockEntity, Consumer<ValueOutput> outputConsumer) {
+        final var output = TagValueOutput.createWithContext(ProblemReporter.DISCARDING, blockEntity.getLevel().registryAccess());
+        outputConsumer.accept(output);
+        return output.buildResult();
     }
 }
