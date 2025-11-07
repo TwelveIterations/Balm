75dd4d98941ffc991560262957ebd8a7bd1d7f93
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-07 22:40:17 +0100

    refactor: Split supplemental events from mappings

--- fabric/src/main/java/net/blay09/mods/balm/fabric/event/FabricBalmEventMappings.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/event/FabricBalmEventMappings.java
@@ -1,7 +1,6 @@
 package net.blay09.mods.balm.fabric.event;
 
 import net.blay09.mods.balm.event.Event;
-import net.blay09.mods.balm.event.EventFactory;
 import net.blay09.mods.balm.event.EventPhases;
 import net.blay09.mods.balm.event.callback.ServerEntityTickCallback;
 import net.blay09.mods.balm.event.callback.ServerLevelTickCallback;
@@ -9,7 +8,6 @@ import net.blay09.mods.balm.event.callback.ServerPlayerTickCallback;
 import net.blay09.mods.balm.event.callback.ServerTickCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 
 import java.util.Map;
 
@@ -22,30 +20,6 @@ public class FabricBalmEventMappings {
             EventPhases.HIGHEST, EventPhases.HIGHEST
     );
 
-    private static final Event<ServerPlayerTickCallback> SERVER_PLAYER_TICK_PRE = EventFactory.createArrayBacked(ServerPlayerTickCallback.class, (listeners) -> (player) -> {
-        for (final var listener : listeners) {
-            listener.handle(player);
-        }
-    });
-
-    private static final Event<ServerPlayerTickCallback> SERVER_PLAYER_TICK_POST = EventFactory.createArrayBacked(ServerPlayerTickCallback.class, (listeners) -> (player) -> {
-        for (final var listener : listeners) {
-            listener.handle(player);
-        }
-    });
-
-    private static final Event<ServerEntityTickCallback> SERVER_ENTITY_TICK_PRE = EventFactory.createArrayBacked(ServerEntityTickCallback.class, (listeners) -> (entity) -> {
-        for (final var listener : listeners) {
-            listener.handle(entity);
-        }
-    });
-
-    private static final Event<ServerEntityTickCallback> SERVER_ENTITY_TICK_POST = EventFactory.createArrayBacked(ServerEntityTickCallback.class, (listeners) -> (entity) -> {
-        for (final var listener : listeners) {
-            listener.handle(entity);
-        }
-    });
-
     public static void bind() {
         ServerTickCallback.PRE.setup((phase, it)
                 -> ServerTickEvents.START_SERVER_TICK.register(mapPhase(phase), it::handle));
@@ -55,46 +29,14 @@ public class FabricBalmEventMappings {
                 -> ServerTickEvents.START_WORLD_TICK.register(mapPhase(phase), it::handle));
         ServerLevelTickCallback.POST.setup((phase, it)
                 -> ServerTickEvents.END_WORLD_TICK.register(mapPhase(phase), it::handle));
-
-        ServerTickEvents.START_SERVER_TICK.register(mapPhase(EventPhases.DEFAULT), (MinecraftServer server) -> {
-            if (SERVER_PLAYER_TICK_PRE.hasHandlers()) {
-                for (final var player : server.getPlayerList().getPlayers()) {
-                    SERVER_PLAYER_TICK_PRE.invoker().handle(player);
-                }
-            }
-        });
         ServerPlayerTickCallback.PRE.setup((phase, it)
-                -> SERVER_PLAYER_TICK_PRE.register(mapPhase(phase), it));
-        ServerTickEvents.END_SERVER_TICK.register(mapPhase(EventPhases.DEFAULT), (MinecraftServer server) -> {
-            if (SERVER_PLAYER_TICK_POST.hasHandlers()) {
-                for (final var player : server.getPlayerList().getPlayers()) {
-                    SERVER_PLAYER_TICK_POST.invoker().handle(player);
-                }
-            }
-        });
+                -> FabricBalmSupplementalEvents.SERVER_PLAYER_TICK_PRE.register(mapPhase(phase), it));
         ServerPlayerTickCallback.POST.setup((phase, it)
-                -> SERVER_PLAYER_TICK_POST.register(mapPhase(phase), it));
-
-        ServerTickEvents.START_SERVER_TICK.register(mapPhase(EventPhases.DEFAULT), (MinecraftServer server) -> {
-            if (SERVER_ENTITY_TICK_PRE.hasHandlers()) {
-                for (final var level : server.getAllLevels()) {
-                    for (final var entity : level.getAllEntities()) {
-                        SERVER_ENTITY_TICK_PRE.invoker().handle(entity);
-                    }
-                }
-            }
-        });
-        ServerEntityTickCallback.PRE.setup((phase, it) -> SERVER_ENTITY_TICK_PRE.register(mapPhase(phase), it));
-        ServerTickEvents.END_SERVER_TICK.register(mapPhase(EventPhases.DEFAULT), (MinecraftServer server) -> {
-            if (SERVER_ENTITY_TICK_POST.hasHandlers()) {
-                for (final var level : server.getAllLevels()) {
-                    for (final var entity : level.getAllEntities()) {
-                        SERVER_ENTITY_TICK_POST.invoker().handle(entity);
-                    }
-                }
-            }
-        });
-        ServerEntityTickCallback.POST.setup((phase, it) -> SERVER_ENTITY_TICK_POST.register(mapPhase(phase), it));
+                -> FabricBalmSupplementalEvents.SERVER_PLAYER_TICK_POST.register(mapPhase(phase), it));
+        ServerEntityTickCallback.PRE.setup((phase, it)
+                -> FabricBalmSupplementalEvents.SERVER_ENTITY_TICK_PRE.register(mapPhase(phase), it));
+        ServerEntityTickCallback.POST.setup((phase, it)
+                -> FabricBalmSupplementalEvents.SERVER_ENTITY_TICK_POST.register(mapPhase(phase), it));
     }
 
     public static ResourceLocation mapPhase(ResourceLocation phase) {
---
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/event/FabricBalmSupplementalEvents.java
@@ -0,0 +1,70 @@
+package net.blay09.mods.balm.fabric.event;
+
+import net.blay09.mods.balm.event.Event;
+import net.blay09.mods.balm.event.EventFactory;
+import net.blay09.mods.balm.event.callback.ServerEntityTickCallback;
+import net.blay09.mods.balm.event.callback.ServerPlayerTickCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.MinecraftServer;
+
+public class FabricBalmSupplementalEvents {
+    public static final Event<ServerPlayerTickCallback> SERVER_PLAYER_TICK_PRE = EventFactory.createArrayBacked(ServerPlayerTickCallback.class, (listeners) -> (player) -> {
+        for (final var listener : listeners) {
+            listener.handle(player);
+        }
+    });
+
+    public static final Event<ServerPlayerTickCallback> SERVER_PLAYER_TICK_POST = EventFactory.createArrayBacked(ServerPlayerTickCallback.class, (listeners) -> (player) -> {
+        for (final var listener : listeners) {
+            listener.handle(player);
+        }
+    });
+
+    public static final Event<ServerEntityTickCallback> SERVER_ENTITY_TICK_PRE = EventFactory.createArrayBacked(ServerEntityTickCallback.class, (listeners) -> (entity) -> {
+        for (final var listener : listeners) {
+            listener.handle(entity);
+        }
+    });
+
+    public static final Event<ServerEntityTickCallback> SERVER_ENTITY_TICK_POST = EventFactory.createArrayBacked(ServerEntityTickCallback.class, (listeners) -> (entity) -> {
+        for (final var listener : listeners) {
+            listener.handle(entity);
+        }
+    });
+
+    public static void initialize() {
+        ServerTickEvents.START_SERVER_TICK.register((MinecraftServer server) -> {
+            if (SERVER_PLAYER_TICK_PRE.hasHandlers()) {
+                for (final var player : server.getPlayerList().getPlayers()) {
+                    SERVER_PLAYER_TICK_PRE.invoker().handle(player);
+                }
+            }
+        });
+        ServerTickEvents.END_SERVER_TICK.register((MinecraftServer server) -> {
+            if (SERVER_PLAYER_TICK_POST.hasHandlers()) {
+                for (final var player : server.getPlayerList().getPlayers()) {
+                    SERVER_PLAYER_TICK_POST.invoker().handle(player);
+                }
+            }
+        });
+
+        ServerTickEvents.START_SERVER_TICK.register((MinecraftServer server) -> {
+            if (SERVER_ENTITY_TICK_PRE.hasHandlers()) {
+                for (final var level : server.getAllLevels()) {
+                    for (final var entity : level.getAllEntities()) {
+                        SERVER_ENTITY_TICK_PRE.invoker().handle(entity);
+                    }
+                }
+            }
+        });
+        ServerTickEvents.END_SERVER_TICK.register((MinecraftServer server) -> {
+            if (SERVER_ENTITY_TICK_POST.hasHandlers()) {
+                for (final var level : server.getAllLevels()) {
+                    for (final var entity : level.getAllEntities()) {
+                        SERVER_ENTITY_TICK_POST.invoker().handle(entity);
+                    }
+                }
+            }
+        });
+    }
+}
