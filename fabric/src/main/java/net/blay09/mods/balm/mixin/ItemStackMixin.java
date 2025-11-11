0bee3faa02c49fb19f18fa8c501c104ad7bc7b48
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-11 10:08:22 +0100

    fix: Inject ItemStack.is() at RETURN and consider previous result

--- fabric/src/main/java/net/blay09/mods/balm/mixin/ItemStackMixin.java
+++ fabric/src/main/java/net/blay09/mods/balm/mixin/ItemStackMixin.java
@@ -10,8 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-    @Inject(method = "is(Lnet/minecraft/core/Holder;)Z", at = @At("HEAD"), cancellable = true)
-    private void is(Holder<Item> holder, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(((ItemStack) (Object) this).is(holder.value()));
+    @Inject(method = "is(Lnet/minecraft/core/Holder;)Z", at = @At("RETURN"), cancellable = true)
+    public void is(Holder<Item> holder, CallbackInfoReturnable<Boolean> cir) {
+        //noinspection ConstantValue
+        cir.setReturnValue(cir.getReturnValue() || ((ItemStack) (Object) this).is(holder.value()));
     }
 }
