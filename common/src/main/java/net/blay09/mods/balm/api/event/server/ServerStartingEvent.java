a3465f6ba0be526cd8991f85fe4512af220eda74
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-12 16:44:33 +0100

    refactor: Deprecate ported events

--- common/src/main/java/net/blay09/mods/balm/api/event/server/ServerStartingEvent.java
+++ common/src/main/java/net/blay09/mods/balm/api/event/server/ServerStartingEvent.java
@@ -3,6 +3,10 @@ package net.blay09.mods.balm.api.event.server;
 import net.blay09.mods.balm.api.event.BalmEvent;
 import net.minecraft.server.MinecraftServer;
 
+/**
+ * @deprecated Use {@link net.blay09.mods.balm.event.callback.ServerLifecycleCallback#STARTING} instead.
+ */
+@Deprecated
 public class ServerStartingEvent extends BalmEvent {
     private final MinecraftServer server;
 
--- common/src/main/java/net/blay09/mods/balm/api/event/server/ServerStoppedEvent.java
+++ common/src/main/java/net/blay09/mods/balm/api/event/server/ServerStoppedEvent.java
@@ -2,5 +2,9 @@ package net.blay09.mods.balm.api.event.server;
 
 import net.minecraft.server.MinecraftServer;
 
+/**
+ * @deprecated Use {@link net.blay09.mods.balm.event.callback.ServerLifecycleCallback#STOPPED} instead.
+ */
+@Deprecated
 public record ServerStoppedEvent(MinecraftServer server) {
 }
