ec6aaee4c3e5fafa36fa58983144f7828108a018
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-20 00:55:46 +0100

    feat: Add BalmScreenUtils#removeWidgetIf

--- common/src/main/java/net/blay09/mods/balm/client/gui/screens/BalmScreenUtils.java
+++ common/src/main/java/net/blay09/mods/balm/client/gui/screens/BalmScreenUtils.java
@@ -6,8 +6,16 @@ import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 
+import java.util.function.Predicate;
+
 public class BalmScreenUtils {
     public static <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(Screen screen, T widget) {
         return ((ScreenAccessor) screen).balm$addRenderableWidget(widget);
+    }
+
+    public static void removeWidgetIf(Screen screen, Predicate<Object> widgetPredicate) {
+        ((ScreenAccessor) screen).balm$getChildren().removeIf(widgetPredicate);
+        ((ScreenAccessor) screen).balm$getNarratables().removeIf(widgetPredicate);
+        ((ScreenAccessor) screen).balm$getRenderables().removeIf(widgetPredicate);
     }
 }
