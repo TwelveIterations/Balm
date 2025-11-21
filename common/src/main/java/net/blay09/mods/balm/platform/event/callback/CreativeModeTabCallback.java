40a78b15a2d60c3ab6997ddc7f786e036d86b8e0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-21 11:13:22 +0100

    feat: Add CreativeModeTabCallback.forTab for convenience

--- common/src/main/java/net/blay09/mods/balm/platform/event/callback/CreativeModeTabCallback.java
+++ common/src/main/java/net/blay09/mods/balm/platform/event/callback/CreativeModeTabCallback.java
@@ -1,6 +1,8 @@
 package net.blay09.mods.balm.platform.event.callback;
 
 import net.blay09.mods.balm.platform.event.EventMapper;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.CreativeModeTab;
 
 public interface CreativeModeTabCallback {
@@ -10,6 +12,15 @@ public interface CreativeModeTabCallback {
         void handle(CreativeModeTab tab, CreativeModeTab.Output output);
 
         EventMapper<BuildContents> EVENT = EventMapper.createUnbound("CreativeModeTabCallback.BuildContents");
+
+        static EventMapper<BuildContents> forTab(Identifier identifier) {
+            return BuildContents.EVENT.filter(identifier.toString(), (base) -> (tab, output) -> {
+                final var tabId = BuiltInRegistries.CREATIVE_MODE_TAB.getKey(tab);
+                if (identifier.equals(tabId)) {
+                    base.handle(tab, output);
+                }
+            });
+        }
     }
 
 }
