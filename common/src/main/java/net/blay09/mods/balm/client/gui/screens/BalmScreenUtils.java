3e51f0aad7b7898aad2272e35a0b13595d2f6ea1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-25 18:04:46 +0100

    feat: Backport BalmScreenUtils

---
+++ common/src/main/java/net/blay09/mods/balm/client/gui/screens/BalmScreenUtils.java
@@ -0,0 +1,21 @@
+package net.blay09.mods.balm.client.gui.screens;
+
+import net.blay09.mods.balm.mixin.ScreenAccessor;
+import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
+import net.minecraft.client.gui.screens.Screen;
+
+import java.util.function.Predicate;
+
+public class BalmScreenUtils {
+    public static <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(Screen screen, T widget) {
+        return ((ScreenAccessor) screen).balm$addRenderableWidget(widget);
+    }
+
+    public static void removeWidgetIf(Screen screen, Predicate<Object> widgetPredicate) {
+        ((ScreenAccessor) screen).balm_getChildren().removeIf(widgetPredicate);
+        ((ScreenAccessor) screen).balm_getNarratables().removeIf(widgetPredicate);
+        ((ScreenAccessor) screen).balm_getRenderables().removeIf(widgetPredicate);
+    }
+}
--- common/src/main/java/net/blay09/mods/balm/mixin/ScreenAccessor.java
+++ common/src/main/java/net/blay09/mods/balm/mixin/ScreenAccessor.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.List;
 
@@ -20,4 +21,7 @@ public interface ScreenAccessor {
 
     @Accessor("renderables")
     List<Renderable> balm_getRenderables();
+
+    @Invoker("addRenderableWidget")
+    <T extends GuiEventListener & Renderable & NarratableEntry> T balm$addRenderableWidget(T widget);
 }
