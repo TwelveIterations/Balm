752750c5ce90fa644b0a9b8b4f843df594987624
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-25 18:05:24 +0100

    feat: Backport Balmstrap access point

---
+++ common/src/main/java/net/blay09/mods/balm/Balmstrap.java
@@ -0,0 +1,20 @@
+package net.blay09.mods.balm;
+
+import net.blay09.mods.balm.api.Balm;
+
+import java.util.function.Consumer;
+
+public class Balmstrap {
+    /**
+     * Register a callback to run when Balm is ready. This is for third party mods that do not use Balm but want to interact with it.
+     * <p>
+     * Mods building on Balm should use {@link Balm#initializeMod(String, BalmRuntimeLoadContext, Consumer)} instead.
+     *
+     * @param callback the callback to run when Balm is ready and its methods can be safely accessed.
+     * @see Balm#initializeMod(String, BalmRuntimeLoadContext, Consumer)
+     */
+    public static void onRuntimeAvailable(Runnable callback) {
+        // TODO In the future, we could avoid the potentially early class load here by having the runtime pull from this class instead.
+        Balm.getRuntime().onRuntimeAvailable(callback);
+    }
+}
