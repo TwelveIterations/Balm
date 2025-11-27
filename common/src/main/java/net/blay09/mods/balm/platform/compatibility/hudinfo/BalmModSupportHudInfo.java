b678a690b9d7faa12ff8bac77ba92c4b706019e0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-27 17:30:22 +0100

    feat: Support DeferredBlock in BalmModSupportHudInfo

--- common/src/main/java/net/blay09/mods/balm/platform/compatibility/hudinfo/BalmModSupportHudInfo.java
+++ common/src/main/java/net/blay09/mods/balm/platform/compatibility/hudinfo/BalmModSupportHudInfo.java
@@ -1,10 +1,22 @@
 package net.blay09.mods.balm.platform.compatibility.hudinfo;
 
+import net.blay09.mods.balm.world.level.block.DeferredBlock;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.block.Block;
+
+import java.util.function.Supplier;
 
 public interface BalmModSupportHudInfo {
     void registerGlobalBlockInfo(Identifier identifier, BlockInfoProvider provider);
 
-    void registerBlockInfo(Identifier identifier, Block block, BlockInfoProvider provider);
+    default void registerBlockInfo(Identifier identifier, Block block, BlockInfoProvider provider) {
+        registerBlockInfo(identifier, () -> block, provider);
+    }
+
+    default void registerBlockInfo(Identifier identifier, DeferredBlock block, BlockInfoProvider provider) {
+        registerBlockInfo(identifier, block::asBlock, provider);
+    }
+
+    void registerBlockInfo(Identifier identifier, Supplier<Block> block, BlockInfoProvider provider);
+
 }
--- common/src/main/java/net/blay09/mods/balm/platform/compatibility/hudinfo/internal/CommonBalmModSupportHudInfo.java
+++ common/src/main/java/net/blay09/mods/balm/platform/compatibility/hudinfo/internal/CommonBalmModSupportHudInfo.java
@@ -2,18 +2,24 @@ package net.blay09.mods.balm.platform.compatibility.hudinfo.internal;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import com.google.common.collect.Multimaps;
+import com.mojang.datafixers.util.Pair;
 import net.blay09.mods.balm.platform.compatibility.hudinfo.BalmModSupportHudInfo;
 import net.blay09.mods.balm.platform.compatibility.hudinfo.BlockInfoProvider;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.block.Block;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class CommonBalmModSupportHudInfo implements BalmModSupportHudInfo {
 
-    private final List<BlockInfoProvider> globalBlockInfoProviders = new ArrayList<>();
-    private final Multimap<Block, BlockInfoProvider> blockInfoProviders = ArrayListMultimap.create();
+    private final List<BlockInfoProvider> globalBlockInfoProviders = Collections.synchronizedList(new ArrayList<>());
+    private final List<Pair<Supplier<Block>, BlockInfoProvider>> deferredBlockInfoProviders = Collections.synchronizedList(new ArrayList<>());
+    private final Multimap<Block, BlockInfoProvider> blockInfoProviders = Multimaps.synchronizedListMultimap(ArrayListMultimap.create());
 
     @Override
     public void registerGlobalBlockInfo(Identifier identifier, BlockInfoProvider provider) {
@@ -21,11 +27,16 @@ public class CommonBalmModSupportHudInfo implements BalmModSupportHudInfo {
     }
 
     @Override
-    public void registerBlockInfo(Identifier identifier, Block block, BlockInfoProvider provider) {
-        blockInfoProviders.put(block, provider);
+    public void registerBlockInfo(Identifier identifier, Supplier<Block> block, BlockInfoProvider provider) {
+        deferredBlockInfoProviders.add(Pair.of(block, provider));
     }
 
     public List<BlockInfoProvider> getBlockInfoProviders(Block block) {
+        for (final var deferredBlockInfoProvider : deferredBlockInfoProviders) {
+            blockInfoProviders.put(deferredBlockInfoProvider.getFirst().get(), deferredBlockInfoProvider.getSecond());
+        }
+        deferredBlockInfoProviders.clear();
+
         final var result = new ArrayList<BlockInfoProvider>();
         result.addAll(blockInfoProviders.get(block));
         result.addAll(globalBlockInfoProviders);
