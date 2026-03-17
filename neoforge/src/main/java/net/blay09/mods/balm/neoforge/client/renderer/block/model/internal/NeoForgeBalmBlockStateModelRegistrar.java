1917ec5c2cbbdeaa12834f88339d9f832f8d7fad
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-17 20:48:35 +0100

    feat: Update to Minecraft 26.1-pre-3

--- neoforge/src/main/java/net/blay09/mods/balm/neoforge/client/renderer/block/model/internal/NeoForgeBalmBlockStateModelRegistrar.java
+++ neoforge/src/main/java/net/blay09/mods/balm/neoforge/client/renderer/block/model/internal/NeoForgeBalmBlockStateModelRegistrar.java
@@ -3,7 +3,7 @@ package net.blay09.mods.balm.neoforge.client.renderer.block.model.internal;
 import net.blay09.mods.balm.client.renderer.block.model.DeferredBlockStateModel;
 import net.blay09.mods.balm.client.renderer.block.model.internal.AbstractBalmBlockStateModelRegistrar;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.resources.Identifier;
 import net.neoforged.neoforge.client.event.ModelEvent;
 import net.neoforged.neoforge.client.model.standalone.SimpleUnbakedStandaloneModel;
