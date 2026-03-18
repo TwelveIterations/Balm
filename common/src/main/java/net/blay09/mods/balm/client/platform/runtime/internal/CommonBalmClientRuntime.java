8074b842b9f129e544f152b8b022323fb46cc96d
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-18 13:37:53 +0100

    chore: Fix imports

--- common/src/main/java/net/blay09/mods/balm/client/platform/runtime/internal/CommonBalmClientRuntime.java
+++ common/src/main/java/net/blay09/mods/balm/client/platform/runtime/internal/CommonBalmClientRuntime.java
@@ -1,8 +1,6 @@
 package net.blay09.mods.balm.client.platform.runtime.internal;
 
 import net.blay09.mods.balm.client.platform.module.internal.InternalsClientModule;
-import net.blay09.mods.balm.core.BalmRegistrars;
-import net.blay09.mods.balm.platform.module.internal.InternalsModule;
 import net.blay09.mods.balm.platform.runtime.BalmRuntimeLoadContext;
 import net.blay09.mods.balm.client.platform.module.BalmClientModule;
 import net.blay09.mods.balm.client.BalmClientRegistrars;
