4302d1b0066d9955335c1dc65143cbccff5ce686
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-24 14:25:07 +0100

    feat(26.1): Re-enable ModMenuIntegration

--- fabric/src/main/java/net/blay09/mods/balm/fabric/platform/compatibility/modmenu/internal/ModMenuUtils.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/platform/compatibility/modmenu/internal/ModMenuUtils.java
@@ -2,19 +2,16 @@ package net.blay09.mods.balm.fabric.platform.compatibility.modmenu.internal;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import net.blay09.mods.balm.Balm;
-import net.blay09.mods.balm.fabric.platform.compatibility.config.internal.ClothConfigUtils;
-import net.blay09.mods.balm.fabric.platform.compatibility.config.internal.ConfiguredConfigProvider;
 import org.jspecify.annotations.Nullable;
 
 public class ModMenuUtils {
     @Nullable
     public static ConfigScreenFactory<?> getConfigScreen(String modId) {
         if (Balm.platform().isModLoaded("cloth-config")) {
-            return ClothConfigUtils.getConfigScreen(modId);
+            // TODO return ClothConfigUtils.getConfigScreen(modId);
         } else if (Balm.platform().isModLoaded("configured")) {
-            return parent -> ConfiguredConfigProvider.createConfigScreen(modId, parent);
-        } else {
-            return null;
+            // TODO return parent -> ConfiguredConfigProvider.createConfigScreen(modId, parent);
         }
+        return null;
     }
 }
