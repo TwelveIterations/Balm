babda51b22b82797ca4ad588b5a62ef81681166a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-03 11:30:05 +0200

    refactor: Restore previous OpenScreenEvent#getNewScreen quirk to avoid semantic breaking change

--- common/src/main/java/net/blay09/mods/balm/api/event/client/OpenScreenEvent.java
+++ common/src/main/java/net/blay09/mods/balm/api/event/client/OpenScreenEvent.java
@@ -22,6 +22,6 @@ public class OpenScreenEvent extends BalmEvent {
     }
 
     public @Nullable Screen getNewScreen() {
-        return newScreen;
+        return newScreen != screen ? newScreen : null;
     }
 }
