8f946d4d885954f5396b7c5daa3623a04736517e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-17 08:22:47 +0200

    fix: Fix memory leak in BlockEntityOnLoadCallback #264

--- fabric/src/main/java/net/blay09/mods/balm/fabric/block/entity/BlockEntityOnLoadCallback.java
+++ fabric/src/main/java/net/blay09/mods/balm/fabric/block/entity/BlockEntityOnLoadCallback.java
@@ -1,46 +1,10 @@
 package net.blay09.mods.balm.fabric.block.entity;
 
-import net.blay09.mods.balm.api.block.entity.OnLoadHandler;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-import java.util.*;
+import java.util.Collection;
 
-public class BlockEntityOnLoadCallback {
+public interface BlockEntityOnLoadCallback {
 
-    private static final Map<Level, LevelScope> levelBlockEntities = new WeakHashMap<>();
-
-    public static void scheduleOnLoad(Level level, Collection<BlockEntity> blockEntities) {
-        synchronized (levelBlockEntities) {
-            final var scope = levelBlockEntities.computeIfAbsent(level, (key) -> new LevelScope());
-            if (scope.onLoadRunning) {
-                scope.pendingFreshBlockEntities.addAll(blockEntities);
-            } else {
-                scope.freshBlockEntities.addAll(blockEntities);
-            }
-        }
-    }
-
-    public static void fireOnLoad(Level level) {
-        synchronized (levelBlockEntities) {
-            final var scope = levelBlockEntities.computeIfAbsent(level, (key) -> new LevelScope());
-            scope.freshBlockEntities.addAll(scope.pendingFreshBlockEntities);
-            scope.pendingFreshBlockEntities.clear();
-
-            scope.onLoadRunning = true;
-            for (final var blockEntity : scope.freshBlockEntities) {
-                if (blockEntity instanceof OnLoadHandler handler) {
-                    handler.onLoad();
-                }
-            }
-            scope.freshBlockEntities.clear();
-            scope.onLoadRunning = false;
-        }
-    }
-
-    private static class LevelScope {
-        private final List<BlockEntity> pendingFreshBlockEntities = new ArrayList<>();
-        private final List<BlockEntity> freshBlockEntities = new ArrayList<>();
-        private boolean onLoadRunning;
-    }
+    void balm$scheduleBlockEntityOnLoad(Collection<BlockEntity> blockEntities);
 }
